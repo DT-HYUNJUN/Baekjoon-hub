0859a7d1c8962bccbe3238eb1400904dcb8f0638
DT-HYUNJUN <105034564+DT-HYUNJUN@users.noreply.github.com>
2023-01-15 12:57:36 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -5,7 +5,7 @@ Sum = sum(lst)
 for i in range(9):
     for j in range(i+1, 9):
         if Sum -lst[i] - lst[j] == 100:
-            spy = {lst[i], lst[j]}
+            spy = [lst[i], lst[j]]
 lst_srt = sorted(lst)
 for elem in lst_srt:
     if elem not in spy:        
