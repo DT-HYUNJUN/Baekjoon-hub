b84b074af889d5ec8090e4dac5eaa90778180d6f
DT-HYUNJUN <105034564+DT-HYUNJUN@users.noreply.github.com>
2023-01-17 01:40:54 +0900

    [Silver V] Title: 소수, Time: 700 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.py
+++ 백준/Silver/2581. 소수/소수.py
@@ -1,16 +1,13 @@
 M = int(input())
 N = int(input())
-cnt = 0
 prime = []
-for i in range(M, N+1):
-    status = 0
-    if i > 1:
-        for j in range(2, i):
-            if i % j == 0:
-                status = 1
-        if status == 0:
-            prime.append(i)        
-if sum(prime) > 0:
+for i in range(M, N + 1):
+    for j in range(2, i + 1):
+        if i == j:
+            prime.append(i)
+        if i % j == 0:
+            break
+if prime:
     print(sum(prime))
     print(min(prime))
 else:
