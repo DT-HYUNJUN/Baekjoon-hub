4b071f20a23799b5fc34b3482b90d439a9fabf84
DT-HYUNJUN <105034564+DT-HYUNJUN@users.noreply.github.com>
2023-01-17 01:10:16 +0900

    [Bronze II] Title: 숫자의 개수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -2,10 +2,5 @@ A = int(input())
 B = int(input())
 C = int(input())
 lst = list(str(A*B*C))
-cnt = [0] * 10
-
-for num in lst:
-    cnt[int(num)] += 1
-
-for i in cnt:
-    print(i)     
+for i in range(10):
+    print(lst.count(str(i)))
