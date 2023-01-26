e228107ba1d5a7042d0e41d898c7226f90edb3b6
DT-HYUNJUN <105034564+DT-HYUNJUN@users.noreply.github.com>
2023-01-26 14:00:19 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 76 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,19 +1,9 @@
 T = int(input())
-quarter, dime, nickel, penny = 25, 10, 5, 1
 for _ in range(T):
     C = int(input())
-    cnt = [0, 0, 0, 0]
-    while C != 0:
-        if (C // quarter) > 0:
-            C -= 25
-            cnt[0] += 1
-        elif (C // dime) > 0:
-            C -= 10
-            cnt[1] += 1
-        elif (C // nickel) > 0:
-            C -= 5
-            cnt[2] += 1
-        elif (C // penny) > 0:
-            C -= 1
-            cnt[3] += 1
+    lst = [25, 10, 5, 1]
+    cnt = []
+    for i in lst:
+        cnt.append(C // i)
+        C %= i
     print(*cnt)
