3a33fb0406d5cab89037d705fb3031707c0a6533
DT-HYUNJUN <105034564+DT-HYUNJUN@users.noreply.github.com>
2023-01-27 13:55:39 +0900

    [Bronze II] Title: 나머지, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,7 +1,1 @@
-lst = []
-for i in range(10):
-    num = int(input())
-    res = num % 42
-    if res not in lst:
-        lst.append(res)
-print(len(lst))
+print(len(set([int(input()) % 42 for _ in range(10)])))
