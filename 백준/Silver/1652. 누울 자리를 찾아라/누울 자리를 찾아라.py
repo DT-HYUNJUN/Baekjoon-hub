2849cdb9d70ddd7448518056bee31bdecaf26d70
DT-HYUNJUN <105034564+DT-HYUNJUN@users.noreply.github.com>
2023-01-31 14:27:26 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -2,15 +2,15 @@ N = int(input())
 room = [[x for x in input()] for _ in range(N)]
 cnt_r, cnt_c = 0, 0
 space_r, space_c = 0, 0
-for i in room:
-    for j in i:
-        if j == '.':
+for i in range(N):
+    for j in range(N):
+        if room[i][j] == '.':
             cnt_r += 1
         else:
-            if cnt_r > 1:
+            if cnt_r >= 2:
                 space_r += 1
             cnt_r = 0
-    if cnt_r > 1:
+    if cnt_r >= 2:
         space_r += 1
     cnt_r = 0
 for i in range(N):
@@ -18,10 +18,10 @@ for i in range(N):
         if room[j][i] == '.':
             cnt_c += 1
         else:
-            if cnt_c > 1:
+            if cnt_c >= 2:
                 space_c += 1
             cnt_c = 0
-    if cnt_c > 1:
+    if cnt_c >= 2:
         space_c += 1
     cnt_c = 0
 print(space_r, space_c)
