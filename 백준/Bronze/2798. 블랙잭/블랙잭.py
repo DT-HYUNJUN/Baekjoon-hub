a7769108626b0dc6ef762f31c7e22c604d44b4b8
DT-HYUNJUN <105034564+DT-HYUNJUN@users.noreply.github.com>
2023-02-01 10:48:25 +0900

    [Bronze II] Title: 블랙잭, Time: 76 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,10 +1,13 @@
 N, M = map(int, input().split())
-lst = list(map(int, input().split()))
-Sum = []
-for i in range(0, N):
-    for j in range(i+1, N):
+lst = sorted([int(x) for x in input().split()], reverse=True)
+
+maximum = 0
+for i in range(N-2):
+    for j in range(i+1, N-1):
         for k in range(j+1, N):
             num = lst[i]+lst[j]+lst[k]
             if num <= M:
-                Sum.append(num)                
-print(sorted(Sum)[-1])
+                if maximum < num:
+                    maximum = num
+                    break
+print(maximum)
