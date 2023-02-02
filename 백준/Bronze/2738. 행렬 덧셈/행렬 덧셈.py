045d576ba6162718942196445145d1cb5786ee46
DT-HYUNJUN <105034564+DT-HYUNJUN@users.noreply.github.com>
2023-02-02 15:16:42 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,8 +1,5 @@
 N, M = map(int, input().split())
-matrix_A = [[int(x) for x in input().split()] for _ in range(N)]
-matrix_B = [[int(x) for x in input().split()] for _ in range(N)]
-res = [[0]*M for _ in range(N)]
+A = [[int(x) for x in input().split()] for _ in range(N)]
+B = [[int(x) for x in input().split()] for _ in range(N)]
 for i in range(N):
-    for j in range(M):
-        print(matrix_A[i][j] + matrix_B[i][j], end=' ')
-    print()
+    print(*[A[i][j] + B[i][j] for j in range(M)])
