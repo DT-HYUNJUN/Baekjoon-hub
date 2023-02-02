eff2f751392012366db160548f779ef4f0a87106
DT-HYUNJUN <105034564+DT-HYUNJUN@users.noreply.github.com>
2023-02-02 15:17:55 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,3 +1,5 @@
+import sys
+input = sys.stdin.readline
 N, M = map(int, input().split())
 A = [[int(x) for x in input().split()] for _ in range(N)]
 B = [[int(x) for x in input().split()] for _ in range(N)]
