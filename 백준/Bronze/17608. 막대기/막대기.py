e5f8932534ea8f7fb919c368c202d6e775694525
DT-HYUNJUN <105034564+DT-HYUNJUN@users.noreply.github.com>
2023-02-07 14:13:16 +0900

    [Bronze II] Title: 막대기, Time: 80 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -3,8 +3,8 @@ N = int(input())
 lst = [int(sys.stdin.readline()) for _ in range(N)]
 standard = lst[-1]
 cnt = 1
-for i in range(N-1, -1, -1):
-    if  lst[i] > standard:
+for i in reversed(lst):
+    if  i > standard:
         cnt += 1
-        standard = lst[i]
+        standard = i
 print(cnt)
