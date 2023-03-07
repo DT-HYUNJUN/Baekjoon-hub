e7d4469b548350630661b76b72ea7644b284631b
DT-HYUNJUN <angle307@naver.com>
2023-03-07 22:13:20 +0900

    Create 통나무 건너뛰기.py

---
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
@@ -0,0 +1,9 @@
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    lst = sorted([int(x) for x in input().split()])
+    res = 0
+    for i in range(N-2):
+        if lst[i+2] - lst[i] > res:
+            res = lst[i+2] - lst[i]
+    print(res)
