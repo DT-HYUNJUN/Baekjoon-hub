b39be8bfc3c0c50b41a5068c5632f9a6084aa50f
DT-HYUNJUN <angle307@naver.com>
2023-03-13 23:05:22 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.py
@@ -0,0 +1,10 @@
+from itertools import combinations_with_replacement
+N = int(input())
+rome = [1, 5, 10, 50]
+dic = {}
+for i in combinations_with_replacement(range(4), N):
+    sum = 0
+    for j in i:
+        sum += rome[j]
+    dic[sum] = dic.get(0, 1) + 1
+print(len(dic))
