1f4d48ea42223fbaa84d335d2c8d9a8521f9b492
DT-HYUNJUN <angle307@naver.com>
2023-03-28 16:17:06 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1124 ms, Memory: 216556 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,15 +1,33 @@
 N = int(input())
-lst_1 = [int(x) for x in input().split()]
+N_lst = sorted([int(x) for x in input().split()])
 M = int(input())
-lst_2 = [int(x) for x in input().split()]
+M_lst = [int(x) for x in input().split()]
 
-dic = {}
+def binary_search_first(lst, value):
+    start, end = 0, N-1
+    while start <= end:
+        mid = int((start + end) / 2)
+        if lst[mid] == value:
+            end = mid - 1
+        if lst[mid] > value:
+            end = mid - 1
+        if lst[mid] < value:
+            start = mid + 1
+    return end
+def binary_search_last(lst, value):
+    start, end = 0, N-1
+    while start <= end:
+        mid = int((start + end) / 2)
+        if lst[mid] == value:
+            start = mid + 1
+        if lst[mid] > value:
+            end = mid - 1
+        if lst[mid] < value:
+            start = mid + 1
+    return end
 
-for i in lst_1:
-    dic[i] = dic.get(i, 0) + 1
 
-for i in lst_2:
-    if i in dic:
-        print(dic[i], end=' ')
-    else:
-        print(0, end=' ')
+for i in M_lst:
+    first = binary_search_first(N_lst, i)
+    last = binary_search_last(N_lst, i)
+    print(last - first, end=' ')
