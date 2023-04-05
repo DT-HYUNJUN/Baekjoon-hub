2aee74465b2d39d2fe97511cc65a41e14048bd8f
DT-HYUNJUN <angle307@naver.com>
2023-04-06 00:21:22 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
@@ -0,0 +1,29 @@
+p, m = map(int, input().split())
+player = []
+room = []
+for i in range(p):
+    level, name = input().split()
+    level = int(level)
+    if i == 0:
+        room.append([(level, name)])
+        continue
+    enter = False
+    for r in room:
+        if (len(r) < m) and r[0][0] - 10 <= level <= r[0][0] + 10:
+            r.append((level, name))
+            enter = True
+            break
+    if enter == False:
+        room.append([(level, name)])
+
+for i in range(len(room)):
+    if len(room[i]) == m:
+        print('Started!')
+        rm = sorted(room[i], key=lambda x:x[1])
+        for r in rm:
+            print(*r)
+    else:
+        print('Waiting!')
+        rm = sorted(room[i], key=lambda x:x[1])
+        for r in rm:
+            print(*r)
