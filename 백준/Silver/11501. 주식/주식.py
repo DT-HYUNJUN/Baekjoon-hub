e03049cda6800e65da5c6c6812767ba609707700
DT-HYUNJUN <angle307@naver.com>
2023-05-20 18:59:52 +0900

    [Silver II] Title: 주식, Time: 3776 ms, Memory: 171256 KB -BaekjoonHub

---
+++ 백준/Silver/11501. 주식/주식.py
@@ -0,0 +1,11 @@
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    stocks = [int(x) for x in input().split()]
+    mxm = stocks[-1]
+    res = 0
+    for i in range(N-1, -1, -1):
+        if mxm < stocks[i]:
+            mxm = stocks[i]
+        res += mxm - stocks[i]
+    print(res)
